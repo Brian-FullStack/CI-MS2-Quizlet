b127dd08f3a9ff47ab770991fa81acb0f7ffd8c9
Brian-FullStack <brian.fullstack@gmail.com>
2021-10-25 21:57:18 +0100

    Style: Added array of question objects

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ document.getElementById('playArrow').onclick = function () {
     document.getElementById('homeScreen').style.display = 'none';
     document.getElementById('quizPage').style.display = 'block';
 }
+//------------------------------------
+
+//----------------Display a question. 
+/**
+ * When the quiz is started display a question in the innerHTML of the head
+ *  Display the possible quiz answers as radio button selections.
+ */
+let question = document.getElementById('question');
+let homeBtn = document.getElementById('homeBtn');
+let currentQuestion = 0;
+let answerOption = document.querySelector('.answerOption');
+
+function displayQuestion(){
+    for(let i = 0; i < questionList.length; i++){
+        question.innerHTML = questionList[currentQuestion];
+        question;
+    }
+}
+displayQuestion();
